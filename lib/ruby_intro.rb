0e83297cd39dc2b56dcaeb15f010749ce4cd36a7
Patrick Bolger <patmbolger@gmail.com>
2015-10-19 15:51:00 +0000

    all tests pass

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -5,24 +5,29 @@
 # Define a method sum(array) that takes an array of integers 
 # as an argument and returns the sum of its elements. 
 # For an empty array it should return zero.
+
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.inject(:+)
 end
+
 
 # Define a method max_2_sum(array) which takes an array of integers 
 # as an argument and returns the sum of its two largest elements. 
 # For an empty array it should return zero. For an array with just 
 # one element, it should return that element.
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum arr.sort.pop(2)
 end
 
 # Define a method sum_to_n?(array) that takes an array of integers 
 # and an additional integer, n, as arguments and returns true 
 # if any two elements in the array of integers sum to n. 
 # An empty array should sum to zero by definition.
+# (see Array#combination)
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).each {|c| return true if (c[0] + c[1]) == n}
+  false
 end
 
 # Part 2
@@ -30,7 +35,7 @@ end
 # Define a method hello(name) that takes a string representing a name 
 # and returns the string "Hello, " concatenated with the name.
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 # Define a method starts_with_consonant?(s) that takes a string and 
@@ -38,15 +43,17 @@ end
 # (For our purposes, a consonant is any letter other than A, E, I, O, U.) 
 # NOTE: be sure it works for both upper and lower case and for nonletters!
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s =~ /^[a-z&&[^aeiou]]/i
 end
 
 # Define a method binary_multiple_of_4?(s) that takes a string and 
 # returns true if the string represents a binary number that is a 
 # multiple of 4. 
 # NOTE: be sure it returns false if the string is not a valid binary number!
+# See String#to_i(2)
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s =~ /^[01]+$/
+    s.to_i(2) % 4 == 0 
 end
 
 # Part 3
@@ -70,5 +77,16 @@ a price of 20 should format as "$20.00" and a price of
 =end 
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    "$#{sprintf("%.2f", @price)}"
+  end
 end
